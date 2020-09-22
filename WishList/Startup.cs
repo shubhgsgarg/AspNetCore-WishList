ee59ca3bbb04938680e901731019378cd5e0c35f
shubhgsgarg <shubhamgarg210399@gmail.com>
2020-09-22 11:09:36 +0530

    1.2

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -30,7 +30,7 @@ namespace WishList
             }
             else
             {
-                app.UseExceptionHandler("Home/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
             app.UseEndpoints(endpoints =>
